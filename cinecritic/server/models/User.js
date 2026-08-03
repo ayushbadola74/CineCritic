6755d80a9e90bed5a6fa4ed3cdad8d91f39975ac
ayushbadola74 <ayushbadola74@gmail.com>
2026-08-03 12:25:16 +0530

    updated user.js

--- cinecritic/server/models/User.js
+++ cinecritic/server/models/User.js
@@ -32,16 +32,16 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Encrypt password using bcrypt before saving
+// 🔐 Hash password
 userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) {
-    next();
-  }
+  if (!this.isModified('password')) return next();
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-// Match user entered password to hashed password in database
+// 🔑 Match password
 userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
